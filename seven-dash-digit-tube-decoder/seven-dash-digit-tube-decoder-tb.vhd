b1a25689c94e0fb379e7a68c617360a80e89747e
FredericDT <frederic.dt.twh@gmail.com>
2018-12-06 11:12:54 +0800

    implement seven-dash-digit-tube-decoder modified:   seven-dash-digit-tube-decoder-tb.vhd modified:   seven-dash-digit-tube-decoder.vhd

--- seven-dash-digit-tube-decoder/seven-dash-digit-tube-decoder-tb.vhd
+++ seven-dash-digit-tube-decoder/seven-dash-digit-tube-decoder-tb.vhd
@@ -8,27 +8,36 @@ architecture tb of sddtd_tb is
 
 component seven_dash_digit_tube_decoder is 
 port(
+        lt_n, rbi_n: in std_logic;
+        bi_rbo_n: inout std_logic;
         d: in std_logic_vector(3 downto 0);
         o: out std_logic_vector(0 to 6)
 );
 end component seven_dash_digit_tube_decoder;
 
+signal lt_n_t: std_logic;
+signal rbi_n_t: std_logic;
+signal bi_rbo_n_t: std_logic;
 signal d_t: std_logic_vector(3 downto 0);
 signal o_t: std_logic_vector(0 to 6);
 
 begin
 	u_sddtd: seven_dash_digit_tube_decoder port map(
-		d_t, o_t
+		lt_n_t, rbi_n_t, bi_rbo_n_t, d_t, o_t
 	);
 
 	process
 		variable err_cnt: integer := 0;
 
 	begin
-	-- case 1
+		lt_n_t <= '1';
+		rbi_n_t <= '1';
 		d_t <= "0000";
 		wait for 10 ns;
 		
+		rbi_n_t <= '0';
+		wait for 10 ns;
+
 		d_t <= "0001";
                 wait for 10 ns;		
 		    
@@ -55,6 +64,31 @@ begin
                     
                 d_t <= "1001";
                 wait for 10 ns;
+
+		d_t <= "1010";
+                wait for 10 ns;
+
+                d_t <= "1011";
+                wait for 10 ns;
+
+                d_t <= "1100";
+                wait for 10 ns;
+
+                d_t <= "1101";
+                wait for 10 ns;
+
+                d_t <= "1110";
+                wait for 10 ns;
+
+                d_t <= "1111";
+                wait for 10 ns;
+
+                --bi_rbo_n_t <= null;
+		lt_n_t <= '0';
+		wait for 10 ns;
+		
+		bi_rbo_n_t <= '0';
+		wait for 10 ns;
 
 		wait;
 	end process;
--- seven-dash-digit-tube-decoder/seven-dash-digit-tube-decoder.vhd
+++ seven-dash-digit-tube-decoder/seven-dash-digit-tube-decoder.vhd
@@ -3,7 +3,8 @@ use ieee.std_logic_1164.all;
 
 entity seven_dash_digit_tube_decoder is 
 port(
-	--lt_n, bi_rbo_n, rbi_n: in std_logic;
+	lt_n, rbi_n: in std_logic;
+	bi_rbo_n: inout std_logic;
 	d: in std_logic_vector(3 downto 0);
 	o: out std_logic_vector(0 to 6)	
 );
@@ -11,13 +12,54 @@ end entity seven_dash_digit_tube_decoder;
 
 architecture sddtd_func of seven_dash_digit_tube_decoder is
 begin
-	--process
-	o(0) <= '1' when d = "0000" or d = "0010" or d = "0011" or d = "0111" or d = "1000" or d = "1001" else '0';
-	o(1) <= '0' when d = "0101" or d = "0110" else '1';
-	o(2) <= '0' when d = "0010" else '1';
-	o(3) <= '0' when d = "0001" or d = "0100" or d = "0111" or d = "1001" else '1';
-	o(4) <= '1' when d = "0000" or d = "0010" or d = "0110" or d = "1000" else '0';
-	o(5) <= '0' when d = "0001" or d = "0010" or d = "0011" or d = "0111" else '1';
-	o(6) <= '0' when d = "0000" or d = "0001" or d = "0111" else '1';	
-	--end process
+	process
+	begin
+		if (bi_rbo_n = '1' and lt_n = '1') then
+			if rbi_n = '1' and d = "0000" then
+				o <= "1111110";
+			elsif d = "0001" then
+				o <= "0110000";
+			elsif d = "0010" then
+				o <= "1101101";
+			elsif d = "0011" then 
+				o <= "1111001";
+			elsif d = "0100" then 
+				o <= "0110011";
+			elsif d = "0101" then
+				o <= "1011011";
+			elsif d = "0110" then 
+				o <= "0011111";
+			elsif d = "0111" then
+				o <= "1110000";
+			elsif d = "1000" then
+				o <= "1111111";
+			elsif d = "1001" then
+				o <= "1110011";
+			elsif d = "1010" then
+				o <= "0001101";
+			elsif d = "1011" then
+				o <= "0011001";
+			elsif d = "1100" then
+				o <= "0110011";
+			elsif d = "1101" then
+				o <= "1001011";
+			elsif d = "1110" then
+				o <= "0001111";
+			elsif d = "1111" then
+				o <= "0110000";
+			end if;
+		elsif bi_rbo_n = '0' or (lt_n = '1' and rbi_n = '0') then
+			o <= "0000000";
+		elsif lt_n <= '0' then
+			o <= "1111111";
+			bi_rbo_n <= '1';
+		end if;
+	--o(0) <= '1' when d = "0000" or d = "0010" or d = "0011" or d = "0111" or d = "1000" or d = "1001" else '0';
+	--o(1) <= '0' when d = "0101" or d = "0110" else '1';
+	--o(2) <= '0' when d = "0010" else '1';
+	--o(3) <= '0' when d = "0001" or d = "0100" or d = "0111" or d = "1001" else '1';
+	--o(4) <= '1' when d = "0000" or d = "0010" or d = "0110" or d = "1000" else '0';
+	--o(5) <= '0' when d = "0001" or d = "0010" or d = "0011" or d = "0111" else '1';
+	--o(6) <= '0' when d = "0000" or d = "0001" or d = "0111" else '1';	
+	end process;
 end architecture sddtd_func;
